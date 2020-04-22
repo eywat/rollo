0cd327bf7f517b852134602df07801ecd2ef93dd
Edward Waterman <edward.waterman@posteo.de>
2020-04-22 19:43:19 +0200

    Switched to COGs

--- rollo.py
+++ rollo.py
@@ -3,129 +3,165 @@ import logging
 
 import numpy as np
 from environs import Env
-from discord.ext.commands import Bot
+from discord.ext.commands import Bot, Cog, command, group
 
+logger = logging.getLogger('Rollo')
 
 class Rollo(Bot):
 
     def __init__(self, command_prefix, description=None, **options):
         super().__init__(command_prefix, description=description, **options)
-        self.history = {}
-        self.meier_history = {}
-
-logger = logging.getLogger('Rollo')
-bot = Rollo(("!", "?", "->"))
 
 
-@bot.event
-async def on_ready():
-    logger.info('Logged in as %s: %d', bot.user.name, bot.user.id)
+class General(Cog): 
+    """ General functions """
+
+    def __init__(self, bot):
+        super().__init__()
+        self.bot = bot
+        self.history: dict = {}
 
 
-@bot.command()
-async def ping(ctx):
-    """ Ping the bot """
-    await ctx.send('pong')
+    @command()
+    async def ping(self, ctx):
+        """ Ping the bot """
+        await ctx.send('pong')
 
 
-@bot.command(aliases=['r'])
-async def roll(ctx, dice: str, history: dict = bot.history):
-    """Rolls a dice in NdN format, depending on prefix: !, ?, ->. (Alt command: r)"""
-    if ctx.prefix == '!':
-        await ro(ctx, dice, history)
-    elif ctx.prefix == '?':
-        await rh(ctx, dice, history)
-    elif ctx.prefix == '->':
-        await rp(ctx, dice, history)
+    @command(aliases=['r'])
+    async def roll(self, ctx, dice: str):
+        """Rolls a dice in NdN format, depending on prefix: !, ?, ->. (Alt command: r)"""
+        if ctx.prefix == '!':
+            await General.ro(ctx, dice, self.history)
+        elif ctx.prefix == '?':
+            await General.rh(ctx, dice, self.history)
+        elif ctx.prefix == '->':
+            await General.rp(ctx, dice, self.history)
 
 
-@bot.command()
-async def ro(ctx, dice: str, history: dict = bot.history):
-    """ Roll a dice in NdN format openly """
-    logger.debug("Rolling open")
-    result = await _r(ctx, dice, history)
-    if not result: 
-        return 
-    await ctx.send(result)
+    @command(aliases=['s'])
+    async def show(self, ctx):
+        """ Show the last thrown dice, even hidden ones. (Alt command: s) """
+        guild = ctx.guild if ctx.guild else 'default'
+        roll = self.history.get(guild, None)
+        if roll is None:
+            await ctx.send('Nobody rolled yet')
+        else:
+            result = ', '.join(map(str, roll))
+            await ctx.send(result)
 
 
-@bot.command()
-async def rh(ctx, dice: str, history: dict = bot.history):
-    """ Roll a dice in NdN format hidden (PM) """
-    logger.debug("Rolling hidden")
-    result = await _r(ctx, dice, history)
-    if not result: 
-        return 
-    await ctx.send(f'{ctx.message.author.mention} rolled hidden!')
-    await ctx.message.author.send(result)
+    @command()
+    async def choose(self, ctx, *choices: str):
+        """Choose between multiple choices."""
+        await ctx.send(random.choice(choices))
 
-
-@bot.command()
-async def rp(ctx, dice: str, history: dict = bot.history):
-    """ Roll a dice in NdN format hidden, no info is shown """
-    logger.debug("Rolled and passed")
-    result = await _r(ctx, dice, history)
-    if not result: 
-        return 
-    await ctx.send(f'{ctx.message.author.mention} rolled hidden and passed on!')
-
-
-async def _r(ctx, dice: str, history: dict = bot.history):
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return None
-    if rolls > 200 or limit > 200: 
-        await ctx.send("Fuck off! Use propper numbers")
-        return None
-
-    result = np.random.randint(low=1, high=limit+1, size=rolls)
-    result = np.sort(result)[::-1]
-    logger.debug(result)
-    guild = ctx.guild if ctx.guild else 'default'
-    logger.debug(guild)
-    history[guild] = result
-    result = ', '.join(map(str, result))
-    return result
-
-
-@bot.command(aliases=['s'])
-async def show(ctx, history=bot.history):
-    """ Show the last thrown dice, even hidden ones. (Alt command: s) """
-    guild = ctx.guild if ctx.guild else 'default'
-    roll = history.get(guild, None)
-    if roll is None:
-        await ctx.send('Nobody rolled yet')
-    else:
-        result = ', '.join(map(str, roll))
+    @staticmethod
+    async def ro(ctx, dice: str, history: dict):
+        """ Roll a dice in NdN format openly """
+        logger.debug("Rolling open")
+        result = await General._r(ctx, dice, history)
+        if not result: 
+            return 
         await ctx.send(result)
 
 
-@bot.command()
-async def choose(ctx, *choices: str):
-    """Choose between multiple choices."""
-    await ctx.send(random.choice(choices))
+    @staticmethod
+    async def rh(ctx, dice: str, history: dict):
+        """ Roll a dice in NdN format hidden (PM) """
+        logger.debug("Rolling hidden")
+        result = await General._r(ctx, dice, history)
+        if not result: 
+            return 
+        await ctx.send(f'{ctx.message.author.mention} rolled hidden!')
+        await ctx.message.author.send(result)
+
+
+    @staticmethod
+    async def rp(ctx, dice: str, history: dict):
+        """ Roll a dice in NdN format hidden, no info is shown """
+        logger.debug("Rolled and passed")
+        result = await General._r(ctx, dice, history)
+        if not result: 
+            return 
+        await ctx.send(f'{ctx.message.author.mention} rolled hidden and passed on!')
+
+
+    @staticmethod
+    async def _r(ctx, dice: str, history: dict):
+        try:
+            rolls, limit = map(int, dice.split('d'))
+        except Exception:
+            await ctx.send('Format has to be in NdN!')
+            return None
+        if rolls > 200 or limit > 200: 
+            await ctx.send("Fuck off! Use propper numbers")
+            return None
+
+        result = np.random.randint(low=1, high=limit+1, size=rolls)
+        result = np.sort(result)[::-1]
+        logger.debug(result)
+        guild = ctx.guild if ctx.guild else 'default'
+        logger.debug(guild)
+        history[guild] = result
+        result = ', '.join(map(str, result))
+        return result
 
 
 
-@bot.group(aliases=['m'])
-async def meier(ctx):
-    """ Roll two hidden d6. Result is stored in extra history (Alt command: m) """
-    if ctx.invoked_subcommand is None:
-        await rh(ctx, '2d6', bot.meier_history)
+
+class Meiern(Cog): 
+    """ Functions to play Meiern """
+
+    def __init__(self):
+        super().__init__()
+        self.history = {}
 
 
-@meier.command(aliases=['h'])
-async def hoeher(ctx):
-    """ Roll two hidden d6 and pass on. (Alt command: h) """
-    await rp(ctx, '2d6', bot.meier_history)
+    @group(aliases=['m'])
+    async def meier(self, ctx):
+        """ Roll two hidden d6. Result is stored in extra history (Alt command: m) """
+        if ctx.invoked_subcommand is None:
+            await General.rh(ctx, '2d6', self.history)
 
 
-@meier.command(aliases=['z'])
-async def zeig(ctx):
-    """ Show the last thrown dice in the round. (Alt command: z)"""
-    await show(ctx, bot.meier_history)
+    @meier.command(aliases=['h'])
+    async def hoeher(self, ctx):
+        """ Roll two hidden d6 and pass on. (Alt command: h) """
+        await General.rp(ctx, '2d6', self.history)
+
+
+    @meier.command(aliases=['z'])
+    async def zeig(self, ctx):
+        """ Show the last thrown dice in the round. (Alt command: z)"""
+        guild = ctx.guild if ctx.guild else 'default'
+        roll = self.history.get(guild, None)
+        if roll is None:
+            await ctx.send('Nobody rolled yet')
+        else:
+            result = ', '.join(map(str, roll))
+            await ctx.send(result)
+
+
+
+
+
+def create_bot() -> Bot:
+    bot = Rollo(("!", "?", "->"))
+
+    async def on_ready():
+        logger.info('Logged in as %s: %d', bot.user.name, bot.user.id)
+
+    async def on_command_error(ctx, error): 
+        logger.warning(f"Command error: {error}")
+
+    bot.add_listener(on_ready, 'on_ready')
+    bot.add_listener(on_command_error, 'on_command_error')
+    bot.add_cog(General(bot))
+    bot.add_cog(Meiern())
+
+    return bot
+
 
 def setup_logger(log_level: int):
     global logger
@@ -145,6 +181,8 @@ def main():
     env.read_env()
 
     setup_logger(env.int('LOG_LEVEL', logging.INFO))
+
+    bot = create_bot()
 
     logger.debug('Starting bot')
     bot.run(env('BOT_TOKEN'))
